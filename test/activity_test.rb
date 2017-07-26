a704a0852e12dcf8ad131b75b8978f9401a28845
Boots2291 <buteramv@gmail.com>
2017-07-26 09:03:11 -0600

    Add first test

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,11 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def test_it_exists
+    activity = Activity.new("Brunch")
+
+    assert_instance_of 
+  end
+
 end
